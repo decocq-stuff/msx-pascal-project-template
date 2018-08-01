e9f348bc873a0db33e5163608fd4edff59c15263
decocq <40805586+decocq@users.noreply.github.com>
2018-08-01 21:41:54 +0200

    Major bugfix build
    Allow compilation in vDos instead of DOSBox after critical bug was found in DOSBox's way of opening Pascal files.
    You should install vDos (www.vdos.info) and set the path to it in global_var.
    Includes other changes that were in the pipeline: no more TEMP files, comment in global_var, optional settings for Emulator launch after compilation.

--- TemplateV1/global_var.bat
+++ TemplateV1/global_var.bat
@@ -1,7 +1,37 @@
-set SourceFile=PROGRAM
-set DiskName=PROGRAM.dsk
-set COMname=PROGRAM
-set Source=source
-set Tools=bin
-set Build=build
+rem This file sets the variables for your project, you must adjust these:
+
+rem ** File name of your program without extension (.PAS assumed)
+    set SourceFile=PROGRAM
+rem ** File name of disk file output; no disk created if blank
+rem ** MSX-DOS1 is also copied to the image
+    set DiskName=PROGRAM.dsk
+rem ** File name of MSX-DOS executable without extension (.COM assigned)
+    set COMname=PROGRAM
+rem ** If set to True, an autoexec.bat is created in the build folder to run %comname%.com
+rem ______not yet implemented, autoexec is always made, and command.com/msxdos.sys always copied
+    set MakeAutoexec=True
+
+rem **VDOS should be present on the system; define path\executable here:
+rem ** If not set or not found, automatic fallback to DOSBox
+rem ** VDOS is available at https://www.vdos.info/download.html
+
+    set vDos=c:\vdos\vdos.exe
+
+rem **Path to your emulator. Only starts after compilation, if the executable exists:
+rem Example: set emulPath="C:\Program Files\OpenMSX\OpenMSX.exe"
+
+    set emulPath=
+
+rem **Additional parameters to pass to the emulator
+rem **Do not use the "-diska" option; it is added automatically if the disk file exists
+rem Example: set emulArgs=-machine Philips_NMS_8255
+
+    set emulArgs=
+
+rem ********** System input/output folder structure/ 
+rem ********** do not change unless you really really have to.
+    set Source=source
+    set Tools=bin
+    set Build=build
+
 echo %date%-%time%-[global_var]: Global Variables set
--- TemplateV1/make.bat
+++ TemplateV1/make.bat
@@ -1,24 +1,24 @@
 @echo off
-REM *** MAKE for MSX Turbo Pascal by S. de Cocq 2018
 call global_var.bat
 set compile=%Tools%\tp3.exe
 set disktool=%Tools%\dsktool_new.exe
 set CopyToFloppy=%disktool% A %Build%\%DiskName%
-
-Echo %date%-%time%-[make]: Creating TEMP file
-copy %source%\%SourceFile%.PAS %source%\TEMP.PAS /y
-
-Echo %date%-%time%-[make]: Creating batch file for DOSBox
+if exist %source%\error.txt del %source%\error.txt
+if exist %vdos% goto :vDOScompile
+echo %date%-%time%-[make]: WARNING: there is a problem with DOSBOX that may cause
+echo %date%-%time%-[make]: nonsense error messages and fail compilation.
+echo %date%-%time%-[make]: Please install VDOS: https://www.vdos.info/download.html
+echo %date%-%time%-[make]: and set the path to it in global_var.bat.
+Echo %date%-%time%-[make]: [Fallback attempt] Creating batch file for DOSBox
 
 echo set path=c:\%Tools% > DOSComp.bat
 echo c: >> DOSComp.bat
 echo cd %Source% >> DOSComp.bat
-echo tp3 TEMP /H /S2E9B /ED200 /Rerror.txt >> DOSComp.bat
+echo tp3 %SourceFile% /H /S2E9B /ED200 /Rerror.txt >> DOSComp.bat
 echo exit >> DOSComp.bat
 
-Echo %date%-%time%-[make]: Launching DOSBox
-
-%Tools%\DOSBOX.exe DOSComp.bat -conf %Tools%\dosbox.conf -noconsole - -exit
+Echo %date%-%time%-[make]: [Fallback attempt] Launching DOSBox
+%Tools%\DOSBOX.exe DOSComp.bat -conf %Tools%\dosbox.conf -noconsole
 if exist stderr.txt (
 	type stderr.txt
 	del stderr.txt
@@ -28,20 +28,33 @@ if exist stdout.txt (
 	del stdout.txt
 )
 del DOSComp.bat
+goto :ErrorHandler
+:vDOScompile
+Echo %date%-%time%-[make]: Creating batch file for vDOS
 
+echo set path=c:\%Tools% > autoexec.txt
+echo c: >> autoexec.txt
+echo cd %Source% >> autoexec.txt
+echo tp3 %SourceFile% /H /S2E9B /ED200 /Rerror.txt >> autoexec.txt
+echo exit >> autoexec.txt
+Echo %date%-%time%-[make]: Attempting to start vDOS
+%vdos% 
+
+del autoexec.txt
+:ErrorHandler
 if exist %source%\error.txt (
 	echo %date%-%time%-[make]: Compiler error:
 	call :OutputErrorStandard
-	
-	del %source%\error.txt
-	if exist %source%\TEMP.PAS del %source%\TEMP.PAS
-	if exist %source%\TEMP.CHN del %source%\TEMP.CHN
-	exit
+	if exist %source%\%SourceFile%.CHN del %source%\%SourceFile%.CHN
+	goto :eof
 ) else (
-	if exist %Build%\%DiskName% del %Build%\%DiskName%
 	Echo %date%-%time%-[make]: Composing COM file chain
-	copy %Tools%\runtime.com/b + %source%\TEMP.chn/b %Build%\%COMname%.com /y
+	copy %Tools%\runtime.com/b + %source%\%SourceFile%.chn/b %Build%\%COMname%.com /y
+	Echo %date%-%time%-[make]: Cleanup
+	del %source%\%SourceFile%.CHN
+	if [%DiskName%]==[] goto :runemulator
 	Echo %date%-%time%-[make]: Building virtual floppy 
+	if exist %Build%\%DiskName% del %Build%\%DiskName%
 	%CopyToFloppy% %Tools%\msxdos.sys %Tools%\command.com
 	rem  Optional! Create autoexec.bat file
 	echo %comname% > %Build%\AUTOEXEC.BAT
@@ -49,15 +62,17 @@ if exist %source%\error.txt (
 	%CopyToFloppy% %Build%\%COMname%.com
 	rem All other files to be included on the virtual floppy:
 	for %%I in (resources\*.*) do %CopyToFloppy% %%I
-	rem %CopyToFloppy% resources\*.*
-	Echo %date%-%time%-[make]: Cleanup
-	del %source%\TEMP.PAS
-	del %source%\TEMP.CHN
-
-rem	call "C:\Program Files\OpenMSX\OpenMSX.exe" -diska %build%\%diskname% -machine Philips_NMS_8255
-
-	goto :eof
+	
 )
+
+:runEmulator
+if [%emulpath%]==[] (
+    Echo %date%-%time%-[make]: No emulator Launching
+) else (
+    Echo %date%-%time%-[make]: Attempt to launch emulator
+	if exist %emulpath% call %emulPath% -diska %build%\%diskname% %emulArgs%
+)
+goto :eof
 :OutputErrorStandard
 rem Format output similar to C and JS compilers for handling by problemmatcher in tasks.json
 		< %source%\error.txt (
@@ -67,12 +82,8 @@ rem Format output similar to C and JS compilers for handling by problemmatcher i
 		set /p ErrorCode=
 		set /p ErrorMsg=
 	)
-	if "%ErrFile%"=="TEMP.PAS"  (
+	
 		echo %source%/%SourceFile%.PAS:%ErrLine%:%ErrColumn%:error:%ErrorMsg%-%ErrorCode%
-		goto :eof
-	) else (
-		echo %source%/%ErrFile%:%ErrLine%:%ErrColumn%:error:%ErrorMsg% -%ErrorCode%
-		goto :eof
-	)
+	
 
 :eof
